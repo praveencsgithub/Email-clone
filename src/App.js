b99f71b418d90072de0b0d746b2e19f38735da84
praveencs <praveencs0047@gmail.com>
2023-10-22 22:53:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='list'>
+    <li>Filter By:</li>
+    <li>Unread</li>
+    <li>Read</li>
+    <li>Favourites</li>
     </div>
-  );
+    <div className='box'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    <div className='box2'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    <div className='box3'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    <div className='box4'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    <div className='box5'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    <div className='box6'>
+    <circle className='circle'><span className='f'>F</span></circle>
+     <h1 className='h1'><span>From:</span>Foo Bar foo.bar@gmail.com </h1>
+     <h2 className='h2'><span>Subject:</span>Lorem ipsum</h2>
+     <h3 className='h3'>Anneee ut odioo eu risus sollicitum veuheh vejebi veij ante</h3>
+     <h4 className='h4'>26/2/2020 10:30am</h4>
+    </div>
+    </div>
+    
+  )
 }
 
 export default App;
